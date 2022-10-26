b6e275f76ae29a34bf5d04b56709d9b9c621e6c5
scworkbd <websitework155@outlook.com>
2022-10-26 23:23:24 +0600

    Download App

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -4,7 +4,7 @@ import { trpc } from "../utils/trpc"
 
 import { useSession } from "next-auth/react"
 import { TbCurrencyTaka } from "react-icons/tb"
-import { BiChevronRight } from "react-icons/bi"
+import { BiChevronDown, BiChevronRight, BiDownload } from "react-icons/bi"
 
 const Balance = () => {
   const { data: session } = useSession()
@@ -24,12 +24,21 @@ const Balance = () => {
             {user?.balance.toFixed(2)}
           </h1>
 
-          <Link href="/user/history">
-            <a className="rounded-full px-1 py-1 border-2 text-xs flex items-center gap-2 pl-3 mt-3">
-              ইতিহাস{" "}
-              <BiChevronRight className="text-xl bg-black/5 rounded-full" />
-            </a>
-          </Link>
+          <div className="flex items-center flex-wrap gap-3">
+            <Link href="/user/history">
+              <a className="rounded-full px-1 py-1 border-[1px] border-rose-900 text-xs flex items-center gap-2 pl-3 mt-3">
+                ইতিহাস{" "}
+                <BiChevronRight className="text-xl bg-black/5 rounded-full" />
+              </a>
+            </Link>
+
+            <Link href="/sweetwork.apk">
+              <a className="border-[1px] border-rose-900 rounded-full px-1 py-1 text-xs flex items-center gap-2 pl-3 mt-3">
+                App ডাউনলোড{" "}
+                <BiDownload className="text-lg bg-black/5 rounded-full animate-bounce -mb-1" />
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
